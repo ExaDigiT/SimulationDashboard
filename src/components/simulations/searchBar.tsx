b79dc3eae5a335dcc4977f5fcc397dff0aabd3c0
Jake Webb <webbtj@ornl.gov>
2024-02-19 14:47:37 -0500

    made place holder components for filters and search

---
+++ src/components/simulations/searchBar.tsx
@@ -0,0 +1,9 @@
+export function SearchBar() {
+  return (
+    <div>
+      <label>Search</label>
+      <input type="text" />
+      {/**  TODO: Add in search magnifying glass icon */}
+    </div>
+  );
+}
