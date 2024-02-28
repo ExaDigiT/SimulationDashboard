281af0f475c258e5ecedc2cde454ffeff4d0ff05
Jake Webb <webbtj@ornl.gov>
2024-02-28 11:44:07 -0500

    Updated a couple of shared component styles

--- src/components/shared/number.tsx
+++ src/components/shared/number.tsx
@@ -3,6 +3,7 @@ import { HTMLProps } from "react";
 export interface NumberInputProps extends HTMLProps<HTMLDivElement> {
   label: string;
   inputProps: HTMLProps<HTMLInputElement>;
+  error?: { error: boolean; errorText: string };
 }
 
 export function NumberInput(props: NumberInputProps) {
@@ -13,9 +14,14 @@ export function NumberInput(props: NumberInputProps) {
         {...props.inputProps}
         type="number"
         className={
-          props.inputProps.className + " border-2 rounded-md px-2 py-2"
+          props.inputProps.className +
+          " border-2 rounded-md px-2 py-2 " +
+          `${props.error?.error} border-red-600`
         }
       />
+      {props.error?.error && (
+        <span className="text-red-600">{props.error.errorText}</span>
+      )}
     </div>
   );
 }
