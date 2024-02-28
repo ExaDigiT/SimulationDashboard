a44cb02f8ccd398e9d0dfd9aefc4dec6db99fc70
Jake Webb <webbtj@ornl.gov>
2024-02-28 11:43:47 -0500

    Updated clases for constructors and added enum for list operators

--- src/models/Scheduler.model.ts
+++ src/models/Scheduler.model.ts
@@ -1,10 +1,30 @@
 import { Job } from "./Job.model";
 
-export interface Scheduler {
+export type JobsMode = "replay" | "random" | "custom";
+
+export interface IScheduler {
   enabled: boolean;
   down_nodes: string[];
-  jobs_mode: "replay" | "custom" | "random";
+  jobs_mode: JobsMode;
   jobs: Job[];
   seed: number | null;
   num_jobs: number | null;
+}
+
+export class Scheduler implements IScheduler {
+  enabled: boolean;
+  down_nodes: string[];
+  jobs: Job[];
+  jobs_mode: JobsMode;
+  num_jobs: number | null;
+  seed: number | null;
+
+  constructor() {
+    this.down_nodes = [];
+    this.enabled = false;
+    this.jobs = [];
+    this.jobs_mode = "replay";
+    this.num_jobs = null;
+    this.seed = null;
+  }
 }
