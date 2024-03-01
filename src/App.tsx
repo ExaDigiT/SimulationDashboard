19052ff6c19088f731667bce117fa7e0082acba2
Jake Webb <webbtj@ornl.gov>
2024-03-01 12:05:09 -0500

    Added in key cloak handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import Keycloak, { KeycloakConfig } from "keycloak-js";
+import { createContext } from "react";
 
 import { routeTree } from "./routeTree.gen";
+import { LoadingSpinner } from "./components/shared/loadingSpinner";
 
-const router = createRouter({ routeTree });
+const initOptions: KeycloakConfig = {
+  url: "https://obsidian.ccs.ornl.gov/auth/",
+  realm: "obsidian",
+  clientId: "obsidian-public",
+};
+
+const kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: "login-required",
+  redirectUri: "http://localhost:8080",
+}).then(
+  (auth) => {
+    if (auth) {
+      if (kc.token) {
+        localStorage.setItem("exadigitAuthToken", kc.token);
+      }
+    }
+  },
+  () => {
+    console.error("Authenticated Failed");
+  }
+);
+
+export interface RouterContext {
+  queryClient: QueryClient;
+}
+
+const queryClient = new QueryClient();
+const router = createRouter({
+  routeTree,
+  context: { queryClient },
+  defaultPendingComponent: () => LoadingSpinner,
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -11,13 +47,15 @@ declare module "@tanstack/react-router" {
   }
 }
 
-const queryClient = new QueryClient();
+export const AppContext = createContext({ AuthToken: kc.token });
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <AppContext.Provider value={{ AuthToken: kc.token }}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AppContext.Provider>
   );
 }
 
