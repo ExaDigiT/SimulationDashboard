41e1dcf9175811803a6cf3599d7ae4c0579e82f8
Jake Webb <webbtj@ornl.gov>
2024-03-06 00:53:08 -0500

    Updated datepicker component to use new package

--- src/components/shared/datepicker.tsx
+++ src/components/shared/datepicker.tsx
@@ -1,25 +1,34 @@
-import { ChangeEvent } from "react";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import { parseISO } from "date-fns";
 
 export interface DatePickerProps {
   label?: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (value: string | null) => void;
   value: string;
 }
 
-export function DatePicker(props: DatePickerProps) {
+export function SharedDatePicker(props: DatePickerProps) {
+  const [date, setDate] = useState(props.value ? parseISO(props.value) : null);
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       {props.label && (
-        <label htmlFor="start-date" className="mb-2 pl-3">
+        <label htmlFor="start-date" className="mb-2 pl-3 text-neutral-200">
           {props.label}
         </label>
       )}
-      <input
-        type="datetime-local"
-        id="start-date"
-        className="border-2 rounded-md px-2 py-2"
-        onChange={props.onChange}
-        value={props.value}
+      <DatePicker
+        selected={date}
+        onChange={(newDate, e) => {
+          e?.preventDefault();
+          setDate(newDate);
+          props.onChange(newDate?.toISOString() || null);
+        }}
+        showTimeSelect
+        className="border-2 rounded-md px-2 py-2 w-full"
+        minDate={new Date()}
+        dateFormat="MM/dd/yyyy, HH:mm z"
       />
     </div>
   );
