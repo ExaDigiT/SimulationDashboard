0b77011ebbf4f80e3828e9c1bdf1e31abbf68dd9
Jake Webb <webbtj@ornl.gov>
2024-03-06 02:02:57 -0500

    removed temp placeholder text

--- src/components/simulations/list/SimulationsDataGridHistogram.tsx
+++ src/components/simulations/list/SimulationsDataGridHistogram.tsx
@@ -1,5 +1,3 @@
 export function SimulationsDataGridHistogram() {
-  return (
-    <div className="flex-1 border-b-2 border-neutral-700">placeholder</div>
-  );
+  return <div className="flex-1 border-b-2 border-neutral-700"></div>;
 }
