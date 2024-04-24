489e630205b887f2f107843d463d020920f286fd
Jake Webb <webbtj@ornl.gov>
2024-04-23 21:43:04 -0400

    Fixed a bug on the simulation list filters

--- src/components/simulations/list/SimulationsDataGridFilters.tsx
+++ src/components/simulations/list/SimulationsDataGridFilters.tsx
@@ -4,7 +4,7 @@ import {
   TrashIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { BaseSyntheticEvent, useState } from "react";
 import { Modal } from "../../shared/modal";
 import { Button } from "../../shared/button";
 import { ColumnHeader } from "../../../models/dataGrid/columnHeader.model";
@@ -28,14 +28,19 @@ export function SimulationsDataGridFilter({
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   const [openColumn, setOpenColumn] = useState(0);
-  const [updatedColumns, setUpdatedColumns] = useState(
-    structuredClone(columns)
-  );
+  const [updatedColumns, setUpdatedColumns] = useState<ColumnHeader[]>([]);
 
   const currentColumn = updatedColumns[openColumn];
   const isActiveFilters = columns.some((column) =>
-    column.activeFilters.some((filter) => filter.value)
+    column.activeFilters.some((filter) => filter.value),
   );
+
+  const onOpen = (e: BaseSyntheticEvent) => {
+    e.preventDefault();
+    setIsOpen(true);
+    const newColumns = structuredClone(columns);
+    setUpdatedColumns(newColumns);
+  };
 
   const onClose = (reset?: boolean) => {
     setIsOpen(false);
@@ -50,7 +55,7 @@ export function SimulationsDataGridFilter({
       updatedColumns.map((column) => ({
         ...column,
         activeFilters: column.activeFilters.filter((filter) => !!filter),
-      }))
+      })),
     );
     navigate({
       search: () => {
@@ -77,17 +82,14 @@ export function SimulationsDataGridFilter({
   return (
     <>
       <button
-        className={`flex items-center justify-center flex-1 w-full gap-2 hover:opacity-75 duration-500 transition-all border-l-2 border-b-2 border-neutral-400 dark:border-neutral-700 dark:text-neutral-200 ${isActiveFilters && "bg-blue-500 text-white border-blue-500 hover:bg-blue-500 hover:opacity-75"}`}
-        onClick={(e) => {
-          e.preventDefault();
-          setIsOpen(true);
-        }}
+        className={`flex w-full flex-1 items-center justify-center gap-2 border-b-2 border-l-2 border-neutral-400 transition-all duration-500 hover:opacity-75 dark:border-neutral-700 dark:text-neutral-200 ${isActiveFilters && "border-blue-500 bg-blue-500 text-white hover:bg-blue-500 hover:opacity-75"}`}
+        onClick={onOpen}
       >
-        <AdjustmentsHorizontalIcon className="w-4 h-4" />
+        <AdjustmentsHorizontalIcon className="h-4 w-4" />
         <span>Filters</span>
       </button>
       <Modal open={isOpen}>
-        <header className="flex items-center justify-between px-4 py-2 border-b-2">
+        <header className="flex items-center justify-between border-b-2 px-4 py-2">
           <span className="text-lg font-medium">Simulation Filter Options</span>
           <button
             onClick={(e) => {
@@ -99,12 +101,12 @@ export function SimulationsDataGridFilter({
             <XMarkIcon className="h-6 w-6" />
           </button>
         </header>
-        <section className="flex-1 flex overflow-auto">
-          <div className="flex flex-col border-r-2 w-1/4">
+        <section className="flex flex-1 overflow-auto">
+          <div className="flex w-1/4 flex-col border-r-2">
             {columns.map((column, index) => (
               <button
                 key={column.propertyName}
-                className={`font-medium px-4 py-2 border-b-2 hover:opacity-50 duration-500 transition-opacity items-start flex ${index === openColumn && "bg-blue-500 text-white hover:opacity-100"}`}
+                className={`flex items-start border-b-2 px-4 py-2 font-medium transition-opacity duration-500 hover:opacity-50 ${index === openColumn && "bg-blue-500 text-white hover:opacity-100"}`}
                 onClick={(e) => {
                   e.preventDefault();
                   setOpenColumn(index);
@@ -114,11 +116,11 @@ export function SimulationsDataGridFilter({
               </button>
             ))}
           </div>
-          <div className="flex-1 flex flex-col px-8 py-4 gap-4 overflow-auto">
+          <div className="flex flex-1 flex-col gap-4 overflow-auto px-8 py-4">
             {currentColumn?.activeFilters.map((filter, index) => (
               <div
                 key={`${openColumn}-filter-${index}`}
-                className="flex gap-4 items-center"
+                className="flex items-center gap-4"
               >
                 <Select
                   choices={currentColumn.operators.map((operator) => ({
@@ -146,7 +148,7 @@ export function SimulationsDataGridFilter({
                     onChange={(value: string | null) => {
                       const newColumns = [...updatedColumns];
                       const column = newColumns[openColumn];
-                      if (value) {
+                      if (value !== null) {
                         column.activeFilters[index].value = value;
                       }
                       setUpdatedColumns(newColumns);
@@ -159,7 +161,7 @@ export function SimulationsDataGridFilter({
                   />
                 </div>
                 <button
-                  className="ml-auto hover:opacity-75 transition-opacity duration-500"
+                  className="ml-auto transition-opacity duration-500 hover:opacity-75"
                   onClick={(e) => {
                     e.preventDefault();
                     const newColumns = [...updatedColumns];
@@ -171,7 +173,7 @@ export function SimulationsDataGridFilter({
                   data-tooltip-content={"Delete Filter"}
                   data-tooltip-delay-show={500}
                 >
-                  <TrashIcon className="w-5 h-5 text-red-600" />
+                  <TrashIcon className="h-5 w-5 text-red-600" />
                 </button>
                 <Tooltip id="delete-filter-tooltip" />
               </div>
@@ -191,12 +193,12 @@ export function SimulationsDataGridFilter({
                 setUpdatedColumns(newColumns);
               }}
             >
-              <PlusIcon className="w-4 h-4 text-green-500" />
+              <PlusIcon className="h-4 w-4 text-green-500" />
               <span>Add New Filter</span>
             </button>
           </div>
         </section>
-        <footer className="border-t-2 flex items-center justify-end py-2 px-4 gap-4">
+        <footer className="flex items-center justify-end gap-4 border-t-2 px-4 py-2">
           <Button
             type="button"
             onClick={(e) => {
