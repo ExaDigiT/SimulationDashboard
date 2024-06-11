37aa112ccd03ccc33470cbeb5aaf94c53bf2f228
Jake Webb <webbtj@ornl.gov>
2024-06-11 15:30:11 -0400

    added an about placeholder

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,5 +5,37 @@ export const Route = createFileRoute("/about")({
 });
 
 function About() {
-  return <div>About</div>;
+  return (
+    <section className="flex w-full justify-center">
+      <div className="flex max-w-lg flex-col justify-center gap-16">
+        <div className="flex gap-8">
+          <p className="dark:text-neutral-200">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum
+            officia reprehenderit voluptates deleniti, vel laudantium
+            blanditiis, debitis quo numquam eveniet expedita nihil atque
+            distinctio aut repudiandae, soluta culpa recusandae?
+          </p>
+          <span className="dark:text-neutral-200 ">image placeholder</span>
+        </div>
+        <div className="flex flex-row-reverse gap-8">
+          <p className="dark:text-neutral-200">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum
+            officia reprehenderit voluptates deleniti, vel laudantium
+            blanditiis, debitis quo numquam eveniet expedita nihil atque
+            distinctio aut repudiandae, soluta culpa recusandae?
+          </p>
+          <span className="dark:text-neutral-200 ">image placeholder</span>
+        </div>
+        <div className="flex gap-8">
+          <p className="dark:text-neutral-200">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum
+            officia reprehenderit voluptates deleniti, vel laudantium
+            blanditiis, debitis quo numquam eveniet expedita nihil atque
+            distinctio aut repudiandae, soluta culpa recusandae?
+          </p>
+          <span className="dark:text-neutral-200 ">image placeholder</span>
+        </div>
+      </div>
+    </section>
+  );
 }
