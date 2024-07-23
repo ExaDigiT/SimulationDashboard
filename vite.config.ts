58f14e0fd6bfa5a35d7bc8b17d0462122ae94cc2
Jesse Hines <hinesjr@ornl.gov>
2024-07-23 10:41:37 -0400

    Update deploy to use env files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH,
-  plugins: [react(), TanStackRouterVite()],
-  server: {
-    port: 8080,
-  },
-});
+export default defineConfig(({ mode }) => {
+  const env = {...process.env, ...loadEnv(mode, process.cwd())};
+  console.log(env)
+  return {
+    base: env.VITE_BASE_PATH,
+    plugins: [react(), TanStackRouterVite()],
+    server: {
+      port: 8080,
+    },
+}});
