b2f655347da9787925f2b67c66f1b5b43991ad79
Jesse Hines <hinesjr@ornl.gov>
2024-10-21 15:19:41 -0400

    Add some helper date methods

--- src/util/datetime.ts
+++ src/util/datetime.ts
@@ -1,7 +1,40 @@
-import { format } from "date-fns";
+import { format, addSeconds, differenceInSeconds, } from "date-fns";
+
+type DateLike = Date|number|string
+
 
 export function convertDateTimeString(datetime: string | null) {
   if (!datetime) return "-";
 
   return format(new Date(datetime).toISOString(), "MM/dd/yyyy, HH:mm z");
+}
+
+export const floorDate = (d: DateLike, interval: number, origin: DateLike) => {
+  let secs = differenceInSeconds(d, origin)
+  secs = secs - (secs % interval)
+  return addSeconds(origin, secs)
+}
+
+export const ceilDate = (d: DateLike, interval: number, origin: DateLike) => {
+  let secs = differenceInSeconds(d, origin)
+  const remainder = secs % interval
+  if (remainder > 0) {
+    secs = secs + (interval - remainder)
+  }
+  return addSeconds(origin, secs)
+}
+
+export const snapDate = (d: DateLike, start: DateLike, end: DateLike, interval: number) => {
+  const secsEnd = differenceInSeconds(end, start)
+  let secs = differenceInSeconds(d, start)
+  secs = Math.min(Math.max(0, secs), secsEnd) // clamp to range
+
+  const remainder = secs % interval
+  if (remainder < interval / 2 || secs + interval >= secsEnd) {
+    secs = secs - remainder
+  } else {
+    secs = secs + (interval - remainder)
+  }
+
+  return addSeconds(start, secs)
 }
