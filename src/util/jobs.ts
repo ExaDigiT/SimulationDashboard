3f208647d247faad33c6f9138cfc0d22a89f5dfa
Jesse Hines <hinesjr@ornl.gov>
2024-10-28 16:53:01 -0400

    Fix bug in computeJobState

--- src/util/jobs.ts
+++ src/util/jobs.ts
@@ -1,6 +1,6 @@
 import { Job } from "../models/Job.model";
 import { DateLike } from "./datetime";
-import { isBefore, isAfter } from "date-fns";
+import { isBefore, isAfter, isEqual } from "date-fns";
 
 export const computeJobState = (job: Job, timestamp?: DateLike) => {
   if (timestamp) {
@@ -9,7 +9,8 @@ export const computeJobState = (job: Job, timestamp?: DateLike) => {
     } else if (!job.time_start || isBefore(timestamp, job.time_start)) {
       return "PENDING";
     } else if (
-      isAfter(timestamp, job.time_start) && (!job.time_end || isBefore(timestamp, job.time_end))
+      (isAfter(timestamp, job.time_start) || isEqual(timestamp, job.time_start)) &&
+      (!job.time_end || isBefore(timestamp, job.time_end))
     ) {
       return "RUNNING";
     } else {
